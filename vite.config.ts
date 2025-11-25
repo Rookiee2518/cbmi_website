d0bf8b60b2eed2dc59776eee073983d04d269565
Sekhar123github <sekhar.chelluri2001@gmail.com>
2025-11-25 14:23:54 +0530

    Fix: Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/cbmi_website/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
